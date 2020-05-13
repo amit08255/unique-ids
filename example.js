b5dbac2b8d0af7d9c5f280996b3b88586b3233b9
amit08255 <amitcute3@gmail.com>
2020-05-13 12:13:25 +0530

    Added string unique ID generator.

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
 const IntegerId = require('./integer-id');
+const stringId = require('./string-id');
 const integerId = new IntegerId();
 
 console.log(integerId.encode(1, 2, 3));
@@ -11,3 +12,10 @@ console.log(integerId.encode(['1', '2', '3'])) // o2fXhV
 console.log(integerId.encode([1n, 2n, 3n])) // o2fXhV
 // Hex notation BigInt:
 console.log(integerId.encode([0x1n, 0x2n, 0x3n])) // o2fXhV
+
+
+/**
+ * String unique ID generator examples
+ */
+
+ console.log(stringId("secrets"));
