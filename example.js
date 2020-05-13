a774df7608f07c33eadb5bfd91e5886c560d2927
amit <amit@schooglink.com>
2020-05-13 11:53:12 +0530

    Added examples.

--- example.js
+++ example.js
@@ -2,3 +2,12 @@ const IntegerId = require('./integer-id');
 const integerId = new IntegerId();
 
 console.log(integerId.encode(1, 2, 3));
+console.log(integerId.encode(1, 2, 3)) // o2fXhV
+console.log(integerId.encode([1, 2, 3])) // o2fXhV
+// strings containing integers are coerced to numbers:
+console.log(integerId.encode('1', '2', '3')) // o2fXhV
+console.log(integerId.encode(['1', '2', '3'])) // o2fXhV
+// BigInt support:
+console.log(integerId.encode([1n, 2n, 3n])) // o2fXhV
+// Hex notation BigInt:
+console.log(integerId.encode([0x1n, 0x2n, 0x3n])) // o2fXhV
